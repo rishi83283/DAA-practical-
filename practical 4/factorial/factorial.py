b5a65d49a54531b52e2a81da0cb94768d4f14c05
rishi <rishi83283@gmail.com>
2026-08-12 22:39:05 +0530

    Simplify practical 4 code by removing main function

--- practical 4/factorial/factorial.py
+++ practical 4/factorial/factorial.py
@@ -10,40 +10,15 @@ Space Complexity:
 
 def iterative_factorial(n):
     fact = 1
-
     for i in range(1, n + 1):
         fact *= i
-
     return fact
-
 
 def recursive_factorial(n):
     if n == 0 or n == 1:
         return 1
-
     return n * recursive_factorial(n - 1)
 
-
-def main():
-    n = int(input("Enter a number: "))
-
-    print("\nchoose")
-    print("1. Iterative Method")
-    print("2. Recursive Method")
-
-    choice = int(input("\nEnter your choice: "))
-
-    if choice == 1:
-        ans = iterative_factorial(n)
-        print(f"\nFactorial = {ans}")
-
-    elif choice == 2:
-        ans = recursive_factorial(n)
-        print(f"\nFactorial = {ans}")
-
-    else:
-        print("Invalid Choice")
-
-
-if __name__ == "__main__":
-    main()
+n = 5
+print("Iterative Factorial:", iterative_factorial(n))
+print("Recursive Factorial:", recursive_factorial(n))
