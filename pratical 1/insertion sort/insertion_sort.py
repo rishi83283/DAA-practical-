0fa2a4043f235e7dbf1dc558faaa85369efc3a08
rishi <rishi83283@gmail.com>
2026-08-12 22:09:15 +0530

    Add practical 1 sorting algorithms

---
+++ pratical 1/insertion sort/insertion_sort.py
@@ -0,0 +1,15 @@
+# Time Complexity: Best: O(n), Average: O(n^2), Worst: O(n^2)
+# Space Complexity: O(1)
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+arr = [12, 11, 13, 5, 6]
+print(insertion_sort(arr))
